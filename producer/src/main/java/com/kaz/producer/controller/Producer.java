c087ed3a7982a280377aba99f79b2467cb29f06f
adibhasan <hasan07023@gmail.com>
2022-05-13 01:31:03 +0600

    Write json response into a json file

--- producer/src/main/java/com/kaz/producer/controller/Producer.java
+++ producer/src/main/java/com/kaz/producer/controller/Producer.java
@@ -1,7 +1,5 @@
 package com.kaz.producer.controller;
 
-import javax.jms.Queue;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
